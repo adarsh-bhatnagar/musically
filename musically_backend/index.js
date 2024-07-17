2cb2663797728ef2df74210a8f94798e194b0cc6
Adarsh Bhatnagar <147133154+adarsh-bhatnagar@users.noreply.github.com>
2024-07-17 22:04:49 +0530

    Update index.js

--- musically_backend/index.js
+++ musically_backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 const port = 8000;
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: 'https://musically-frontend.onrender.com/', // Allow requests from this origin
     methods: ['GET', 'POST'], // Allow these HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
